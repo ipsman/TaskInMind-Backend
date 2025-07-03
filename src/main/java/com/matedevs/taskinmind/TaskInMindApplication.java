2a9ce2f7c843b5fd4e727f67c0fd2ac60c20abb7
ipsman <fogkefegaming@gmail.com>
2025-07-03 22:22:49 +0200

    Upload

---
+++ src/main/java/com/matedevs/taskinmind/TaskInMindApplication.java
@@ -0,0 +1,13 @@
+package com.matedevs.taskinmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskInMindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskInMindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matedevs/taskinmind/TaskInMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matedevs.taskinmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskInMindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
